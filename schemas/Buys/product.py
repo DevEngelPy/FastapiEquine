2f1b2485a555985728ee41705481af4a19873863
DevEngelPy <devengel1996@gmail.com>
2024-08-15 19:43:20 -0600

    validaciones de schema product

--- schemas/Buys/product.py
+++ schemas/Buys/product.py
@@ -1,11 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import date
-
-
 class Product_purchase(BaseModel):
-    id:int
-    name:str
-    price:float
-    acount:float
-    total:float
+    id:int = Field(ge=0,le=1000)
+    name:str = Field(min_length=5, max_length=10)
+    price:float = Field(ge=0,le=1000)
+    acount:float = Field(ge=0,le=1000)
+    total:float = Field(ge=0,le=1000)
     created:date
